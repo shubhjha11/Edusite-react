a8f36646f8732e3d37f6ff3039e764d37b2708f3
shubhjha11 <55157642+shubhjha11@users.noreply.github.com>
2021-05-04 15:29:36 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
                     <div className="col-12 max-auto">
                         <nav className="navbar navbar-expand-lg navbar-light bg-light">
                             <div className="container-fluid">
-                                <NavLink className="navbar-brand menu-active ml-5" to="/">Navbar</NavLink>
+                                <NavLink className="navbar-brand menu-active ml-5" to="/">3Bows</NavLink>
                                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
