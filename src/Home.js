23e12163e3fa87d05acc17e5c618ac07005efd8a
shubhjha11 <55157642+shubhjha11@users.noreply.github.com>
2021-05-04 15:44:51 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import Common from './Common';
 const Home = () => {
     return (
         <>
-            <Common name="Welcome to 3bows you need us to mark all you arrows correctly" 
+            <Common name="Welcome to 3bows you need us to mark all you arrows perfectly" 
             imgsrc={web} visit="/services" btname="Get Started" />
         </>
     );
